05a6915f91492396b7ffde17d7dde6631bce2ccc
Joshua Elliott <joshualjelliott@gmail.com>
2023-01-07 15:44:10 +1100

    Add makeshift fence, and change player to RigidBody2D to have intertia and collide with fence.

--- player/Player.gd
+++ player/Player.gd
@@ -1,14 +1,13 @@
-extends Node2D
+extends RigidBody2D
 
 
 # Declare member variables here. Examples:
 # var a: int = 2
 # var b: String = "text"
 
-export var speed = 300
-
 var pickups: Array = []
 
+export var force = 100
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -19,23 +18,19 @@ func _ready() -> void:
 func _process(delta: float) -> void:
    pass
 
-
-func _physics_process(delta: float) -> void:
-    var velocity = Vector2.ZERO
-
+func _integrate_forces(state) -> void:
+    var exert = Vector2.ZERO
     if Input.is_action_pressed('move_down'):
-        move_and_collide(Vector2(0, 1))
+        exert.y = 1
     if Input.is_action_pressed('move_left'):
-        move_and_collide(Vector2(-1, 0))
+        exert.x = -1
     if Input.is_action_pressed('move_right'):
-        move_and_collide(Vector2(1, 0))
+        exert.x = 1
     if Input.is_action_pressed('move_up'):
-        move_and_collide(Vector2(0, -1))
+        exert.y = -1
 
-    velocity = velocity.normalized()
-
-    position += velocity * speed * delta
-
+    exert = exert.normalized()
+    applied_force = exert*force
 
 func _on_Area2D_area_entered(area:Area2D) -> void:
     print('picking up item')
