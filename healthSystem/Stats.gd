6e617597b11e7637f196246fe80497723542f214
Vivian Siobhan Wong <vivee233@gmail.com>
2023-01-07 18:29:02 -0500

    Add score UI script and update stats script to accomodate

--- healthSystem/Stats.gd
+++ healthSystem/Stats.gd
@@ -3,8 +3,12 @@ extends Node
 export(int) var max_health = 3
 onready var health = max_health setget set_health
 
+onready var score = 0 setget set_score
+
 signal no_health
 signal health_changed(value)
+
+signal score_changed(value)
 
 func set_health(value) -> void:
     health = value
@@ -15,3 +19,8 @@ func set_health(value) -> void:
 
 func reset() -> void:
     health = max_health
+    score = 0
+
+func set_score(value):
+    score = value
+    emit_signal("score_changed", score)
