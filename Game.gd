83549493e4a761cd6960620705e799c508f265b4
Adam Chyb <achybby@gmail.com>
2023-01-07 16:58:03 +1100

    Vegetables spawn randomly.

--- Game.gd
+++ Game.gd
@@ -1,14 +1,23 @@
 extends Node2D
 
+var Vegetable = preload('res://vegetable/Vegetable.tscn')
+
 
 # Declare member variables here. Examples:
 # var a: int = 2
 # var b: String = "text"
 
+var spawn_start : Vector2
+var spawn_end : Vector2
+
+onready var field = $Field
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-    pass # Replace with function body.
+    spawn_start.x = field.cell_size.x
+    spawn_start.y = field.cell_size.y
+    spawn_end.x = (field.width - 1) * field.cell_size.x
+    spawn_end.y = (field.height - 1) * field.cell_size.y
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -18,5 +27,13 @@ func _ready() -> void:
 
 func _physics_process(delta: float) -> void:
     #TODO: spawn enemies
-    #TODO: spawn veggies
     pass
+
+
+func _on_VegetableSpawnTimer_timeout() -> void:
+    var new_vegetable = Vegetable.instance()
+
+    new_vegetable.vegetable_type = new_vegetable.VegetableType.values()[rand_range(0, new_vegetable.VegetableType.size())]
+    new_vegetable.position.x = rand_range(spawn_start.x, spawn_end.x)
+    new_vegetable.position.y = rand_range(spawn_start.y, spawn_end.y)
+    add_child(new_vegetable)
