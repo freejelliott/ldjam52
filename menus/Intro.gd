3a867c25a86ee9213e53141b180eae542decf19c
Adam Chyb <achybby@gmail.com>
2023-01-09 18:30:56 +1100

    Click to continue after intro.

--- menus/Intro.gd
+++ menus/Intro.gd
@@ -8,11 +8,12 @@ onready var animation: AnimationPlayer = $AnimationPlayer
 func start_intro() -> void:
     print('starting intro')
     animation.play('text_scroll')
+    animation.queue('continue')
 
 
 func _on_Panel_gui_input(event:InputEvent) -> void:
     if event.is_action_pressed('click'):
-        if animation.is_playing():
+        if animation.current_animation == 'text_scroll':
             animation.seek(animation.current_animation_length)
         else:
             emit_signal('intro_ended')
