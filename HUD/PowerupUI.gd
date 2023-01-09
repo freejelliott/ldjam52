7265e17168ab6a0f4e968c8d6a188679e97bfbef
Vivian Siobhan Wong <vivee233@gmail.com>
2023-01-08 19:04:25 -0500

    Add powerup UI script and update stats script to accomodate

---
+++ HUD/PowerupUI.gd
@@ -0,0 +1,23 @@
+extends Control
+
+onready var label_boots = $LabelBoots
+onready var label_baskets = $LabelBaskets
+
+var powerup_boots = 0
+var powerup_baskets = 0
+
+func set_powerup_boots(value):
+    if label_boots != null:
+        label_boots.text = "Boots: " + str(PlayerStats.powerup_boots)
+
+func set_powerup_baskets(value):
+     if label_baskets != null:
+        label_baskets.text = "Baskets: " + str(PlayerStats.powerup_baskets)
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+    self.powerup_boots = PlayerStats.powerup_boots
+    PlayerStats.connect("boots_changed", self, "set_powerup_boots")
+    self.powerup_baskets = PlayerStats.powerup_baskets
+    PlayerStats.connect("baskets_changed", self, "set_powerup_baskets")
+
--- HUD/Stats.gd
+++ HUD/Stats.gd
@@ -4,11 +4,16 @@ export(int) var max_health = 3
 onready var health = max_health setget set_health
 
 onready var score = 0 setget set_score
+onready var powerup_boots = 0 setget set_powerup_boots
+onready var powerup_baskets = 1 setget set_powerup_baskets
 
 signal no_health
 signal health_changed(value)
 
 signal score_changed(value)
+
+signal boots_changed(value)
+signal baskets_changed(value)
 
 func set_health(value) -> void:
     health = value
@@ -20,7 +25,19 @@ func set_health(value) -> void:
 func reset() -> void:
     health = max_health
     score = 0
+    powerup_boots = 0
+    powerup_baskets = 1
 
 func set_score(value):
     score = value
     emit_signal("score_changed", score)
+    
+# Tracking powerups
+
+func set_powerup_boots(value):
+    powerup_boots = value
+    emit_signal("boots_changed", powerup_boots)
+    
+func set_powerup_baskets(value):
+    powerup_baskets = value
+    emit_signal("baskets_changed", powerup_baskets)
